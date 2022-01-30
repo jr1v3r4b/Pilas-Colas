eb2e86d45f306c493423dbb8d31c63a6261bf14a
Jhon <jhon_rivera82151@elpoli.edu.co>
2022-01-30 09:18:16 -0500

    Initial Commit

---
+++ PILASCONLISTASXCODE/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PILASCONLISTASXCODE
+//
+//  Created by UDP Soporte on 30/01/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
